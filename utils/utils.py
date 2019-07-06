d7a8eb3d5cb16c60f61642e3bda9b8199265e330
ali hedayatnia <a.hedayat.m@gmail.com>
2019-07-07 01:59:46 +0430

    adding document to the utils/utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -13,35 +13,106 @@ import torch
 from optparse import OptionParser
 
 def mkdir(dir_path, dir_name, forced_remove=False):
-	new_dir = '{}/{}'.format(dir_path,dir_name)
-	if forced_remove and os.path.isdir( new_dir ):
-		shutil.rmtree( new_dir )
-	if not os.path.isdir( new_dir ):
-		os.makedirs( new_dir )
+    """
+    Make new directory
+    Parameters
+    ----------
+    dir_path : str
+        path to new directory
+    dir_name : str
+        new directory name
+    forced_remove : boolean, optional
+        if forced_remove is true and there is a directory in dir_path with dir_name then delete it.
+    """
+    new_dir = '{}/{}'.format(dir_path,dir_name)
+    if forced_remove and os.path.isdir( new_dir ):
+        shutil.rmtree( new_dir )
+    if not os.path.isdir( new_dir ):
+        os.makedirs( new_dir )
 
 def touch(file_path, file_name, forced_remove=False):
-	new_file = '{}/{}'.format(file_path,file_name)
-	assert os.path.isdir( file_path ), ' \"{}\" does not exist.'.format(file_path)
-	if forced_remove and os.path.isfile(new_file):
-		os.remove(new_file)
-	if not os.path.isfile(new_file):
-		open(new_file, 'a').close()
+    """
+    Make a new empty file
+    Parameters
+    ----------
+    file_path : str
+        new file path
+    file_name : str
+        new file name
+    forced_remove : boolean, optional
+        if forced_remove is true and there is a file in file_path with file_name then delete it.
+    """
+    new_file = '{}/{}'.format(file_path,file_name)
+    assert os.path.isdir( file_path ), ' \"{}\" does not exist.'.format(file_path)
+    if forced_remove and os.path.isfile(new_file):
+        os.remove(new_file)
+    if not os.path.isfile(new_file):
+        open(new_file, 'a').close()
 
 def write_file(file_path, file_name, content, new_line=True, forced_remove_prev=False):
-	touch(file_path, file_name, forced_remove=forced_remove_prev)
-	with open('{}/{}'.format(file_path, file_name), 'a') as f:
-		f.write('{}'.format(content))
-		if new_line:
-			f.write('\n')
-		f.close()
+    """
+    write a line to the file
+    Parameters
+    ----------
+    file_path : str
+        path to the file that we want to write to it
+    file_name : str
+        name of file that we want to write to it
+    content : str
+        content of new line
+    new_line : boolean, optional
+        adding a new line character to the end of new line or not 
+    forced_remove_prev : boolean, optional
+        if forced_remove_prev is false then append the new line to the file
+    """
+    touch(file_path, file_name, forced_remove=forced_remove_prev)
+    with open('{}/{}'.format(file_path, file_name), 'a') as f:
+        f.write('{}'.format(content))
+        if new_line:
+            f.write('\n')
+        f.close()
 
 def copy_file(src_path, src_file_name, dst_path, dst_file_name):
-	shutil.copyfile('{}/{}'.format(src_path, src_file_name), '{}/{}'.format(dst_path,dst_file_name))  
+    """
+    copying file
+    Parameters
+    ----------
+    src_path : str
+        source file path
+    src_file_name : str
+        source file name
+    dst_path : str
+        destination path
+    dst_file_name : str
+        name of copied file
+    """
+    shutil.copyfile('{}/{}'.format(src_path, src_file_name), '{}/{}'.format(dst_path,dst_file_name))  
 
 def ls(dir_path):
-	return os.listdir(dir_path)
+    """
+    listing directory contents
+    Parameters
+    ----------
+    dir_path : str
+        directory that we want to list its content
+    
+    Returns
+    -------
+    : list of strings
+        contents of directory
+    """
+    return os.listdir(dir_path)
 
 def save_np_file(file_name,np_arr):
+    """
+    saving a numpy array
+    Parameters
+    ----------
+    file_name : str
+        file path and file name that we want to saving a numpy array to it
+    np_arr : numpy.array
+        numpy array that we want to saving it
+    """
     np.save(file_name, np_arr)
 
 def get_args():
